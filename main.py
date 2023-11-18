8896db3cfc98c8981be074974de18803ee60840d
Muhammad Hafiz Azhari <mhafiz.azh@gmail.com>
2023-11-18 13:43:51 -0700

    starter code

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+import finnhub
 
 app = Flask(__name__)
+
+api_key = 'ckuqblpr01qmtr8lgnu0ckuqblpr01qmtr8lgnug'
+finnhub_client = finnhub.Client(api_key=api_key)
+
+def company_data(tick: str) -> dict:
+    pass
+
+def suprise_earn(tick: str) -> dict:
+    pass
+
+def financial_report(tick: str) -> dict:
+    pass
+
+def predicitoin(tick: str):
+    pass
 
 @app.route('/')
 def home():
